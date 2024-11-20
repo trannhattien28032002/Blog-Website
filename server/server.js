672df731b9fa5e3d1de6ad22ee0ac9332b89e062
Tyler Tran <trannhattien28032002@gmail.com>
2024-11-20 07:26:54 +0700

    Update server.js 20-11-2024

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import 'dotenv/config';
+import bcrypt from 'bcryptjs';
 
 const server = express();
 let PORT = 3000;
@@ -17,6 +19,22 @@ server.post("/signup", (req, res) => {
     if (fullname.length < 3) {
         return res.status(403).json({ "error": "Fullname must be at least 3 letters long" })
     }
+
+    if (!email.length) {
+        return res.status(403).json({ "error": "Enter Email" })
+    }
+
+    if (!emailRegex.test(email)) {
+        return res.status(403).json({ "error": "Email is invalid" })
+    }
+
+    if (!passwordRegex.test(password)) {
+        return res.status(403).json({ "error": "Password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letters" })
+    }
+
+    bcrypt.hash(password, 10, (err, hashed_password) => {
+        console.log(hashed_password);
+    })
 
     return res.status(200).json({ "status": "okay" })
 })
